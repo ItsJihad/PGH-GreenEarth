f0b533392e89b6ffc3d979d97c431056381a65d4
itsjihad <itsjihad@proton.me>
2025-09-11 00:22:27 +0600

    API's are irrigated

--- script.js
+++ script.js
@@ -10,39 +10,32 @@ const bamboo = document.getElementById("bamboo-trees");
 const climbers = document.getElementById("climbers-trees");
 const aquatic = document.getElementById("aquatic-trees");
 
-const allButtons=document.querySelectorAll(".catbuttons")
+const allButtons = document.querySelectorAll(".catbuttons");
 
 const productArea = document.getElementById("productContainer");
 const categoryplace = document.getElementById("categoryplace");
 
-const allplantsAPI ="https://openapi.programming-hero.com/api/plants"
-const allcategoriesAPI ="https://openapi.programming-hero.com/api/plants"
-const plantsbycategoriesAPI ="https://openapi.programming-hero.com/api/plants"
-const PlantsDetailAPI ="https://openapi.programming-hero.com/api/plants"
-const categoriesAPI ="https://openapi.programming-hero.com/api/categories"
+const allplantsAPI = "https://openapi.programming-hero.com/api/plants";
+const allcategoriesAPI = "https://openapi.programming-hero.com/api/categories";
+const plantsbycategoriesAPI =
+  "https://openapi.programming-hero.com/api/category/${id}";
+const PlantsDetailAPI = "https://openapi.programming-hero.com/api/plant/${id}";
 
+let url;
+fetching = (url,id) => {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data);
+};
 
-
-
-
-fetching=(url,id)=>{
-    fetch(url)
-    .then(res=>res.json())
-    .then((json)=>producer(json.plants))
-    
-}
-
-
-producer=(plants)=>{
-        
-    for (const plant of plants) {
-        
-        let placeholder=document.createElement("div")
-        placeholder.innerHTML=""
-
-        placeholder.innerHTML=`
-        
-        <div class="p-4 bg-white rounded-lg flex flex-col gap-3 ">
+const handler = (data) => {
+  if ("plants" in data && Array.isArray(data.plants)) {
+    data.plants.forEach((plant) => {
+      const Divplacer = document.createElement("div");
+      Divplacer.innerHTML = "";
+      Divplacer.innerHTML = `
+            
+            <div class="p-4 bg-white rounded-lg flex flex-col gap-3 ">
                 <img src="${plant.image}"  class="h-48  rounded-lg">
                 <div class="flex gap-3">
                 <div class="flex-1 flex flex-col gap-2">
@@ -62,66 +55,63 @@ producer=(plants)=>{
                 Add to Cart
                 </button>
             </div>
-        
-        `
-        productArea.appendChild(placeholder)
+            
+            
+            `;
+      document.getElementById("productContainer").appendChild(Divplacer);
+    });
+  } else if ("categories" in data && Array.isArray(data.categories)) {
+    let Abutton = document.createElement("button");
 
-    }
+    Abutton.classList =
+      "catbuttons self-stretch px-2.5 py-2 active text-black rounded inline-flex justify-start items-center gap-2.5 font-medium font-['Inter'] text-base hover:bg-green-200 transition-colors duration-300";
 
-    }
+    Abutton.innerText = "All Trees";
 
+    Abutton.addEventListener("click", () => {
 
+    const catbuttons = document.querySelectorAll(".catbuttons");
+    catbuttons.forEach((button) => button.classList.remove("active"));
 
-
-
-categoryFetch=(url)=>{
-    fetch(url)
-    .then(res=>res.json())
-    .then((json)=>categorybuttons(json.categories))
+   
+    Abutton.classList.add("active");
     
-}
+    document.getElementById("productContainer").innerHTML=""
+    fetching(allplantsAPI).then(handler);
+  });
 
+    categoryplace.appendChild(Abutton);
 
+    data.categories.forEach((category) => {
+      let Cbutton = document.createElement("button");
+      Cbutton.classList =
+        "catbuttons self-stretch px-2.5 py-2 bg-[#FFFFF] text-black rounded inline-flex justify-start items-center gap-2.5 font-medium font-['Inter'] text-base hover:bg-green-200 transition-colors duration-300";
+      Cbutton.id = category.id;
+      Cbutton.innerText = category.category_name;
+      categoryplace.appendChild(Cbutton);
 
-
-
-
-
-
-categorybuttons=(categories)=>{
-        for (const category of categories) {
-            
-            let catbutton = document.createElement("button")
-            catbutton.innerText=""
-            catbutton.classList="self-stretch px-2.5 py-2 bg-[#F0FDF4] text-black rounded  justify-center items-center gap-4 font-medium font-['Inter'] text-start hover:bg-green-600 transition-colors duration-300 "
-
-            catbutton.innerText=category.category_name
+      Cbutton.addEventListener("click", () => {
+        const catbuttons = document.querySelectorAll(".catbuttons");
+        catbuttons.forEach((button) => {
+          button.classList.remove("active");
+        });
+        Cbutton.classList.add("active");
+        console.log(category.id)
         
-            categoryplace.appendChild(catbutton)
-        
-        
-        document.getElementById("categoryplace").appendChild(catbutton)
-       
-            
-        
-            
-        }
-        
-        
-}
+        document.getElementById("productContainer").innerHTML=""
 
+        const url = `https://openapi.programming-hero.com/api/category/${category.id}`;
+        console.log("Fetching category:", url);
+        fetching(url).then(handler);
 
-
-
-
-
-
-
-
-
+      });
+    });
+  }
+};
 
 window.onload = () => {
-    categoryFetch(categoriesAPI);
-    fetching(allplantsAPI);
+  fetching(allplantsAPI).then(handler);
+  fetching(allcategoriesAPI).then(handler);
 };
+
 
