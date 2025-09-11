fe2c8004dc5c66540a50019203fa25b211f770a3
itsjihad <itsjihad@proton.me>
2025-09-11 22:32:15 +0600

    Just having a back up incase i mess up something

--- script.js
+++ script.js
@@ -3,108 +3,94 @@ const categoryplace = document.getElementById("categoryplace");
 const spinner = document.getElementById("spinner");
 
 // Reusable Fetch
-fetcher=(url)=>{
+fetcher = (url) => {
   document.getElementById("spinner").classList.remove("hidden");
   productArea.innerHTML = "";
   fetch(url)
-  .then(res=>res.json())
-  .then((data)=>{
-    constructor(data)
-  document.getElementById("spinner").classList.add("hidden")
-  
-  })
-  
-  
-}
+    .then((res) => res.json())
+    .then((data) => {
+      constructor(data);
+      document.getElementById("spinner").classList.add("hidden");
+    });
+};
 
 // Category Constructor {PRODUCT CARDS PLACE HOLDER}
 
-constructor=(data)=>{
+constructor = (data) => {
   if ("plants" in data && Array.isArray(data.plants)) {
-    cardsCreator(data.plants)
-  }else if ("categories" in data && Array.isArray(data.categories)){
-    ButtonCreator(data.categories)
+    cardsCreator(data.plants);
+    
+  } else if ("categories" in data && Array.isArray(data.categories)) {
+    ButtonCreator(data.categories);
   }
-}
+};
 
 // ALL Tree Single Button
-allTrees=()=>{
-  const allTreesbutton=document.createElement("button")
-    allTreesbutton.innerHTML=`
+allTrees = () => {
+  const allTreesbutton = document.createElement("button");
+  allTreesbutton.innerHTML = `
       <button class="categorybuttons active self-stretch px-2.5 py-2 text-black rounded inline-flex justify-start items-center gap-2.5 font-medium font-['Inter'] text-base hover:bg-green-200 transition-colors duration-300">All Trees</button>
-    `
-  categoryplace.appendChild(allTreesbutton)
-  allTreesbutton.addEventListener("click",()=>{
-    document.getElementById("productContainer").innerHTML = ""
-    fetcher("https://openapi.programming-hero.com/api/plants")
-    
-  })
-}
-
+    `;
+  categoryplace.appendChild(allTreesbutton);
+  allTreesbutton.addEventListener("click", () => {
+    document.getElementById("productContainer").innerHTML = "";
+    fetcher("https://openapi.programming-hero.com/api/plants");
+  });
+};
 
 // Each Category Button Creator
-ButtonCreator=(buttonData)=>{
+ButtonCreator = (buttonData) => {
+  categoryplace.innerHTML = "";
+  allTrees();
 
-  categoryplace.innerHTML = ""
-  allTrees()
-
-  buttonData.forEach((button)=>{
-
-   
-    const categorybutton=document.createElement("button")
-    categorybutton.classList = "categorybuttons  m-2 sm:m-0 bg-[#F0FDF9] px-2.5 py-2 font-medium font-['Inter'] text-black rounded hover:bg-green-200 ";
+  buttonData.forEach((button) => {
+    const categorybutton = document.createElement("button");
+    categorybutton.classList =
+      "categorybuttons  m-2 sm:m-0 bg-[#F0FDF9] px-2.5 py-2 font-medium font-['Inter'] text-black rounded hover:bg-green-200 ";
     categorybutton.id = button.id;
     categorybutton.textContent = button.category_name;
 
-    categoryplace.appendChild(categorybutton)
-    document.getElementById("mobileCategorialmoal").appendChild(categorybutton.cloneNode(true))
+    categoryplace.appendChild(categorybutton);
+    document
+      .getElementById("mobileCategorialmoal")
+      .appendChild(categorybutton.cloneNode(true));
 
-  const allButtons = document.querySelectorAll(".categorybuttons");
-  ActiveChecker(allButtons)
-  
- 
-  })
+    const allButtons = document.querySelectorAll(".categorybuttons");
+    ActiveChecker(allButtons);
 
-}
+    
+  });
+};
 
 // Button Activity Checker
-ActiveChecker=(buttons)=>{
+ActiveChecker = (buttons) => {
   // Active Class Remover
-    remover=()=>{
-      buttons.forEach(button=>{
-      button.classList.remove("active")
-    })
-    }
-// Button Clicking Function
-    buttons.forEach(button=>{
-      
-      button.onclick=()=>{
-        remover()
-        button.classList.add("active")
+  remover = () => {
+    buttons.forEach((button) => {
+      button.classList.remove("active");
+    });
+  };
+  // Button Clicking Function
+  buttons.forEach((button) => {
+    button.onclick = () => {
+      remover();
+      button.classList.add("active");
 
-    document.getElementById("productContainer").innerHTML = "";
+      document.getElementById("productContainer").innerHTML = "";
       fetcher(`https://openapi.programming-hero.com/api/category/${button.id}`);
-      
-      }
-    })
-
-}
-
-//Modal Pop Up
-
+    };
+  });
+};
 
 
 
 // Cards Creator
 
-cardsCreator=(cardData)=>{
-
-
+cardsCreator = (cardData) => {
   cardData.forEach((card) => {
-    
-      const Divplacer = document.createElement("div");
-      Divplacer.innerHTML = "";
-      Divplacer.innerHTML = `
+    const Divplacer = document.createElement("div");
+    Divplacer.innerHTML = "";
+    Divplacer.innerHTML = `
             
             <div class="p-4 bg-white rounded-lg flex flex-col gap-3 " id="${card.id}">
                 <img src="${card.image}"  class="h-48  rounded-lg">
@@ -129,39 +115,37 @@ cardsCreator=(cardData)=>{
             `;
 
 
- Divplacer.querySelector(".productName").addEventListener("click", () => {
+            //modal Creator
+
+    Divplacer.querySelector(".productName").addEventListener("click", () => {
       document.getElementById("modalTitle").innerText = card.name;
       document.getElementById("modalImage").src = card.image;
-      document.getElementById("modalCategory").innerText = "Category: " + card.category;
-      document.getElementById("modalPrice").innerText = "৳ " + card.price;
-      document.getElementById("modalDescription").innerText = card.description;
+      document.getElementById("modalCategory").innerText = `Category : ${card.category}`
+      document.getElementById("modalPrice").innerText = `Price : ৳ ${card.price}`
+      document.getElementById("modalDescription").innerText =`Description : ${card.description}`
 
       document.getElementById("my_modal_1").showModal();
     });
 
+    document.getElementById("productContainer").appendChild(Divplacer);
+    // ----------------------------
 
+    pricer(card.name,card.price,document.querySelectorAll(".productName"))
 
+  });
+};
 
-            
-      document.getElementById("productContainer").appendChild(Divplacer);
-      // ----------------------------
-      
-
-    });
-  
-     
-}
-
-// Spinner
-
-// bootAnimation=()=>{
-//   document.getElementById("spinner").classList.remove("hidden")
-//   document.getElementById("spinner").classList.add("hidden")
-// }
 
 // Loads on Boot
-window.onload=()=>{
-  fetcher("https://openapi.programming-hero.com/api/plants")
-  fetcher("https://openapi.programming-hero.com/api/categories")
-  
-}
+window.onload = () => {
+  fetcher("https://openapi.programming-hero.com/api/plants");
+  fetcher("https://openapi.programming-hero.com/api/categories");
+};
+
+const mangoName = document.getElementById("mangoName")
+const mangoPrice = document.getElementById("mangoPrice")
+const mangoQuantity = document.getElementById("mangoQuantity")
+const mangoTotal = document.getElementById("mangoTotal")
+const mangoCross = document.getElementById("mangoCross")
+
+
