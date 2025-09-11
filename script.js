e96892bc36bdb549d60ca54f7ee61f3ca9a2f556
itsjihad <itsjihad@proton.me>
2025-09-11 21:08:13 +0600

    Added Boot Animation/Load Spinner

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const productArea = document.getElementById("productContainer");
 const categoryplace = document.getElementById("categoryplace");
+const spinner = document.getElementById("spinner");
 
 // Reusable Fetch
 fetcher=(url)=>{
+  document.getElementById("spinner").classList.remove("hidden");
+  productArea.innerHTML = "";
   fetch(url)
   .then(res=>res.json())
-  .then(data=>constructor(data))
+  .then((data)=>{
+    constructor(data)
+  document.getElementById("spinner").classList.add("hidden")
+  
+  })
+  
+  
 }
 
 // Category Constructor {PRODUCT CARDS PLACE HOLDER}
@@ -40,8 +49,10 @@ ButtonCreator=(buttonData)=>{
   allTrees()
 
   buttonData.forEach((button)=>{
+
+   
     const categorybutton=document.createElement("button")
-    categorybutton.classList = "categorybuttons  m-2 sm:m-0 bg-[#F0FDF9] px-2.5 py-2 text-black rounded hover:bg-green-200 ";
+    categorybutton.classList = "categorybuttons  m-2 sm:m-0 bg-[#F0FDF9] px-2.5 py-2 font-medium font-['Inter'] text-black rounded hover:bg-green-200 ";
     categorybutton.id = button.id;
     categorybutton.textContent = button.category_name;
 
@@ -50,6 +61,7 @@ ButtonCreator=(buttonData)=>{
 
   const allButtons = document.querySelectorAll(".categorybuttons");
   ActiveChecker(allButtons)
+  
  
   })
 
@@ -72,6 +84,7 @@ ActiveChecker=(buttons)=>{
 
     document.getElementById("productContainer").innerHTML = "";
       fetcher(`https://openapi.programming-hero.com/api/category/${button.id}`);
+      
       }
     })
 
@@ -79,19 +92,18 @@ ActiveChecker=(buttons)=>{
 
 //Modal Pop Up
 
-modalPop=()=>{
   document.getElementById("MainModal").innerHTML=`
 
-<div class="p-4 bg-white rounded-lg flex flex-col gap-3 " id="${data.id}">
-    <div class="text-gray-800 text-2xl font-semibold" onclick="my_modal_1.showModal()" >${data.name}</div>
-                <img src="${data.image}"  class="h-48  rounded-lg">
+<div class="p-4 bg-white rounded-lg flex flex-col gap-3 " id="/">
+    <div class="text-gray-800 text-2xl font-semibold" onclick="my_modal_1.showModal()" >/</div>
+                <img src="/"  class="h-48  rounded-lg">
                 <div class="flex flex-col gap-3">
-                <div class="text-gray-800 text-sm font-semibold" onclick="my_modal_1.showModal()" >Category : ${data.category}</div>
+                <div class="text-gray-800 text-sm font-semibold" >Category : /</div>
                 <div class="flex-1 flex flex-col gap-2">
-                  <div class="flex-1 text-left text-gray-800 text-sm font-semibold">৳ Price : ${data.price}</div>
+                  <div class="flex-1 text-left text-gray-800 text-sm font-semibold">৳ Price :/</div>
                     </div>
                     <div class="flex flex-col gap-3">
-                    <div class="text-gray-800 text-sm font-itallic" onclick="my_modal_1.showModal()" >Description : ${data.description}</div>
+                    <div class="text-gray-800 text-sm font-itallic" >Description :/</div>
                 </div>
                 </div>
                 <button class=" Addtocart w-full px-5 py-3 bg-green-700 hover:bg-yellow-300 rounded-full text-white text-base font-medium">
@@ -100,12 +112,12 @@ modalPop=()=>{
             </div>
 
 `
-}
 
 
 // Cards Creator
 
 cardsCreator=(cardData)=>{
+
 
   cardData.forEach((card) => {
     
@@ -117,7 +129,7 @@ cardsCreator=(cardData)=>{
                 <img src="${card.image}"  class="h-48  rounded-lg">
                 <div class="flex gap-3">
                 <div class="flex-1 flex flex-col gap-2">
-                    <div class="text-gray-800 text-sm font-semibold" onclick="my_modal_1.showModal()" >${card.name}</div>
+                    <div class="productName text-gray-800 text-sm font-semibold" onclick="my_modal_1.showModal()" >${card.name}</div>
                     <div class="opacity-80 text-gray-800 text-xs leading-none">
                     ${card.description}
                     </div>
@@ -125,7 +137,7 @@ cardsCreator=(cardData)=>{
                     <div class="px-3 py-1 bg-green-100 rounded-full flex justify-center items-center">
                         <span class="text-green-700 text-sm font-medium">${card.category}</span>
                     </div>
-                    <div class="flex-1 text-right text-gray-800 text-sm font-semibold">৳ ${card.price}</div>
+                    <div class="productPrice flex-1 text-right text-gray-800 text-sm font-semibold">৳ ${card.price}</div>
                     </div>
                 </div>
                 </div>
@@ -136,12 +148,24 @@ cardsCreator=(cardData)=>{
             `;
             
       document.getElementById("productContainer").appendChild(Divplacer);
+      // ----------------------------
+      
 
     });
+  
+     
 }
+
+// Spinner
+
+// bootAnimation=()=>{
+//   document.getElementById("spinner").classList.remove("hidden")
+//   document.getElementById("spinner").classList.add("hidden")
+// }
 
 // Loads on Boot
 window.onload=()=>{
   fetcher("https://openapi.programming-hero.com/api/plants")
   fetcher("https://openapi.programming-hero.com/api/categories")
+  
 }
